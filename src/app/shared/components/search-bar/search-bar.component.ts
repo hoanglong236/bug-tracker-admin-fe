c814bbba5821e87a55424a9c8648cc1244ecd2fb
hoanglong236 <hahahalalala236@gmail.com>
2023-10-28 11:48:48 +0700

    Build Verified module #12: - Add search-bar component to shared module - Fix bug UI: manage-projects & manage-users component

---
+++ src/app/shared/components/search-bar/search-bar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.scss'],
+})
+export class SearchBarComponent {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { PaginatorComponent } from './components/paginator/paginator.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
-const exportComponents = [PaginatorComponent];
+const exportComponents = [PaginatorComponent, SearchBarComponent];
 
 @NgModule({
-  declarations: [PaginatorComponent],
+  declarations: [PaginatorComponent, SearchBarComponent],
   imports: [CommonModule],
   exports: [
     CommonModule,
